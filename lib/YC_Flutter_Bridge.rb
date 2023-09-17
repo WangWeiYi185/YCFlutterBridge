1c42ecc0650616802b9be9e0c2a8b675f4fb50fd
王维一 <weiyi.wang@ly.com>
2023-09-17 22:58:41 +0800

    init

---
+++ lib/YC_Flutter_Bridge.rb
@@ -0,0 +1,6 @@
+require "YC_Flutter_Bridge/version"
+
+module YCFlutterBridge
+  class Error < StandardError; end
+  # Your code goes here...
+end
---
+++ lib/YC_Flutter_Bridge/version.rb
@@ -0,0 +1,3 @@
+module YCFlutterBridge
+  VERSION = "0.1.0"
+end
