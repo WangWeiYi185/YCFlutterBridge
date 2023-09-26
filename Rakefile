92dffc0141dbaaf431930849aacc127d64278d03
王维一 <weiyi.wang@ly.com>
2023-09-27 01:39:28 +0800

    file init agin

--- Rakefile
+++ Rakefile
@@ -1,19 +1,6 @@
-# require "bundler/gem_tasks"
-# require "rspec/core/rake_task"
+require "bundler/gem_tasks"
+require "rspec/core/rake_task"
 
-# RSpec::Core::RakeTask.new(:spec)
+RSpec::Core::RakeTask.new(:spec)
 
-# task :default => :spec
-
-require 'bundler/gem_tasks'
-
-def specs(dir)
-  FileList["spec/#{dir}/*_spec.rb"].shuffle.join(' ')
-end
-
-desc 'Runs all the specs'
-task :specs do
-  sh "bundle exec bacon #{specs('**')}"
-end
-
-task :default => :specs
+task :default => :spec
--- spec/YC_Flutter_Bridge_spec.rb
+++ spec/YC_Flutter_Bridge_spec.rb
@@ -1,4 +1,3 @@
-require File.expand_path('../spec_helper', __FILE__)
 RSpec.describe YCFlutterBridge do
   it "has a version number" do
     expect(YCFlutterBridge::VERSION).not_to be nil
@@ -8,17 +7,3 @@ RSpec.describe YCFlutterBridge do
     expect(false).to eq(true)
   end
 end
-
-
-
-module Pod
-  describe Command::Bridge do
-    describe 'CLAide' do
-      it 'registers it self' do
-        Command.parse(%w{ bridge }).should.be.instance_of Command::Bridge
-      end
-    end
-  end
-end
-
-
